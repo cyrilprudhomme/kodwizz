c60cca7d133b011e01c6f5dbcddb40cb1221f4c3
SIU\extcprudhomme <cyril.prudhomme.ext@cibtp.fr>
2023-02-28 16:01:43 +0100

    Ajout de la prise en charge de la mise a jour

--- src/app/menu/menu.page.ts
+++ src/app/menu/menu.page.ts
@@ -1,14 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import {AngularFireAuth} from "@angular/fire/compat/auth";
 import {Router} from "@angular/router";
-
+import packageJson from 'package.json';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.page.html',
   styleUrls: ['./menu.page.scss'],
 })
 export class MenuPage implements OnInit {
-
+  appVersion = packageJson.version;
   constructor(public auth: AngularFireAuth, private router: Router) {
   }
 
